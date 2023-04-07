2b3826fb02a4ef4820461981c657d48a63161b8a
Jakub Minarik <jakub.minarik.11@gmail.com>
2023-04-07 20:08:10 +0200

    Add missing docs

--- app/src/main/java/cz/minarik/rickandmorty/domain/model/TVCharacter.kt
+++ app/src/main/java/cz/minarik/rickandmorty/domain/model/TVCharacter.kt
@@ -1,5 +1,12 @@
 package cz.minarik.rickandmorty.domain.model
 
+/**
+ * Model for character list item.
+ *
+ * @property id character id
+ * @property name character name
+ * @property imageUrl character image url
+ */
 data class TVCharacter(
     val id: String,
     val name: String?,
